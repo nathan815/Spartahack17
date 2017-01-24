3276e9243d7afbb4f681e1108173a1271922431c
Nathaniel Johnson <nathancj224@gmail.com>
2017-01-24 12:37:26 -0500

    removed test html form

--- backend/edit.php
+++ backend/edit.php
@@ -1,15 +1,3 @@
 <?php
 
-
-?>
-
-<form action="submit.php" method="post">
-    name: <input name="name" /><br>
-    user id: <input name="user_id" /><br>
-    cat: <input name="category" /><br>
-    location desc: <input name="location_desc" /><br>
-    person desc: <input name="person_desc" /><br>
-    long: <input name="long" /><br>
-    lat <input name="lat" /><br>
-    <input type="submit" />
-</form>
+// edit
